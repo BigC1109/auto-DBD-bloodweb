69d3aaf1a11b26f5019aace070e66e2520808be4
BigC1109 <66857091+BigC1109@users.noreply.github.com>
2025-04-08 03:31:22 -0400

    Added several icons | Checks all icons on screen

--- main.py
+++ main.py
@@ -5,19 +5,69 @@ import pyautogui
 import time
 import os
 
-LOCALPATH = "C:\\Users\\Jeffr\\OneDrive\\Desktop\\projects\\auto-DBD-bloodweb\\"
-
 def outOfBloodies():
     return True
 
-def selectPurchase():
-    os.remove('my_screenshot.png')
-    if True:
-        os.remove('region_screenshot.png')
+def purchaseableItems():
     pyautogui.screenshot('my_screenshot.png')
 
+    file_paths = {}
+    for folder, subfolders, filenames in os.walk('icons'):
+        for filename in filenames:
+            file_path = os.path.join(folder, filename)
+            file_paths[filename.strip('.png')] = {"filepath": file_path, "amount": []}
+    for key in file_paths.keys():
+        print(file_paths[key]["filepath"])
+
+    for file in file_paths.keys():
+        try:
+            purchase_location = pyautogui.locateAll(file_paths[file]["filepath"], "my_screenshot.png", confidence=0.7)
+            purchase_location = list(purchase_location)
+        except Exception as e:
+            print(e)
+            purchase_location = None
+        
+        if purchase_location is not None:
+            print("Success!")
+            for i in purchase_location:
+                i = pyautogui.center(i)
+                if file_paths[file]["amount"] != []:
+                    for k in file_paths[file]["amount"]:
+                        point1 = np.array(i)
+                        point2 = np.array(k)
+                        distance = np.linalg.norm(point2 - point1)
+                        if distance >= 10:
+                            file_paths[file]["amount"].append(i)
+                else:
+                    file_paths[file]["amount"].append(i)
+        else:
+            print("failure!")
+    
+    print(file_paths)
+
+    for i in file_paths:
+        for k in file_paths[i]["amount"]:
+            pyautogui.moveTo(k)
+            pyautogui.mouseDown()
+            time.sleep(0.2)
+            pyautogui.mouseUp()
+            time.sleep(3)
+
+
+
+
+def selectPurchase():
+    pyautogui.screenshot('my_screenshot.png')
+
+    file_paths = []
+    for folder, subfolders, filenames in os.walk('icons'):
+        for filename in filenames:
+            file_path = os.path.join(folder, filename)
+            file_paths.append(file_path)
+    print(file_paths)
+
     try:
-        purchase_location = pyautogui.locate("image.png", "my_screenshot.png", confidence=0.1)
+        purchase_location = pyautogui.locate("image.png", "my_screenshot.png", confidence=0.7)
     except pyautogui.ImageNotFoundException as e:
         purchase_location = None
     
@@ -32,11 +82,12 @@ def pickPriority():
     pass
 
 def main():
-    for i in range(5):
+    for i in range(0):
         time.sleep(1)
         print(i)
     
-    selectPurchase()
+    # selectPurchase()
+    purchaseableItems()
     
 
 if __name__ in "__main__":
