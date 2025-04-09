080b5cfcfea4fc89da42c063fcbc780a52232567
BigC1109 <66857091+BigC1109@users.noreply.github.com>
2025-04-09 02:18:59 -0400

    added comments explaining the program

--- main.py
+++ main.py
@@ -6,27 +6,24 @@ import time
 import os
 import pyscreeze
 
-def outOfBloodies():
-    return True
+'''
+This program will prioritize purchasing items in the bloodweb for Dead By Daylight.
+It does this through image recognition of pyautogui. Automatically picking the wanted
+item first, and when that is completed purchases the automatic purchase button.
+'''
 
-def check_mysteryboxes(file, file_paths, purchase_location):
-    if purchase_location is not None:
-        for i in purchase_location:
-            y = i[0]
-            i = pyautogui.center(i)
-            x = i[1]
-            print(pyautogui.pixel(int(x), int(y)))
-            if file_paths[file]["amount"] != []:
-                for k in file_paths[file]["amount"]:
-                    point1 = np.array(i)
-                    point2 = np.array(k)
-                    distance = np.linalg.norm(point2 - point1)
-                    if distance >= 10:
-                        file_paths[file]["amount"].append(i)
-            else:
-                file_paths[file]["amount"].append(i)
+def outOfBloodies():
+    '''
+    Future failsafe for when out of bloodpoints, will just end the program.
+    '''
+    return False
 
 def purchaseableItems():
+    '''
+    This will go through each icon on the screen, and determine what is purchaseable.
+    It will add it to the dictionary file_paths, where it stores the amount of each item
+    there is.
+    '''
     pyautogui.screenshot('my_screenshot.png')
 
     file_paths = {}
@@ -58,6 +55,8 @@ def purchaseableItems():
                 y = i[1]
                 i = pyautogui.center(i)
                 x = i[0]
+
+                # This specific section was made because the mystery_box's look too similar, and need to be color tested as well.
                 if file == "brown_mystery_box":
                     if not (pyautogui.pixelMatchesColor(int(x), int(y), (56, 43, 33), tolerance=20) or pyautogui.pixelMatchesColor(int(x), int(y), (92, 68, 50), tolerance=20)): # Non-grayed out: (92, 68, 50)
                         continue
@@ -72,7 +71,7 @@ def purchaseableItems():
                     if not pyautogui.pixelMatchesColor(int(x), int(y), (77, 38, 81), tolerance=20):
                         continue
                 elif file == "pink_mystery_box":
-                    if not pyautogui.pixelMatchesColor(int(x), int(y), (0, 0, 0), tolerance=10):
+                    if not pyautogui.pixelMatchesColor(int(x), int(y), (209, 13, 111), tolerance=10):
                         continue
 
                 if file_paths[file]["amount"] != []:
@@ -87,21 +86,11 @@ def purchaseableItems():
     
     return file_paths
 
-    # for file in file_paths:
-    #     if file_paths[file]["amount"] != []:
-    #         for i in file_paths[file]["amount"]:
-    #             print(file)
-    # print("============")
-    # for i in file_paths:
-    #     for k in file_paths[i]["amount"]:
-    #         pyautogui.moveTo(k)
-    #         print(i)
-    #         time.sleep(.5)
-
-
-
-
 def selectPurchase(file_paths, prioList):
+    '''
+    This will collect the current prioList, and parse through it using the file_paths from earlier.
+    Using both of these it will purchase items in order of what the user wants.
+    '''
     for item in prioList:
         for file in file_paths:
             if file == item and file_paths[file]["amount"] != []:
@@ -114,7 +103,10 @@ def selectPurchase(file_paths, prioList):
 
 
 def pickPriority():
-    # Improve on system later, create a way to interact with fileIO, allow CLI question system for list
+    '''
+    In the future this will check for the pickprio.csv file, if it exists will get the information from it.
+    If it doesn't exist, it will run through and create one through CLI command prompts.
+    '''
     prioList = ["bloodsense_map", "bloodshot_eye", "anti_hemorrhagic_syringe", "brand_new_part", "odd_bulb", "brown_mystery_box", "green_mystery_box", "purple_medkit", "jigsaw_piece", "auto_purchase"]
     return prioList
 
@@ -124,9 +116,9 @@ def main():
         time.sleep(1)
         print(i)
     
-    while True:
+    prioList = pickPriority()
+    while not outOfBloodies():
         file_paths = purchaseableItems()
-        prioList = pickPriority()
         selectPurchase(file_paths, prioList)
         time.sleep(2)
         pyautogui.moveTo(0, 1000)
